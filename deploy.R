c593b6a15523a01125d88dd20e9d77ad068db08c
tbonza <tylers.pile@gmail.com>
2017-12-05 18:45:04 -0500

    added labels, filters not working, see #7

--- deploy.R
+++
@@ -1,8 +0,0 @@
-library(rsconnect)
-
-
-rsconnect::setAccountInfo(name='ds5110',
-			  token='39F3BA8750A0FC6A059F27AB00D77438',
-			  secret=Sys.getenv("RSHINY_CERT"))
-
-rsconnect::deployApp('ds5110')
